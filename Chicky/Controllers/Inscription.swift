facf3bda56ce2c71a437ff348508e486330e4943
Med Jamel Bouassida <mohamedjameleddine.bouassida@esprit.tn>
2021-11-08 23:52:58 +0100

    Interfaces

---
+++ Chicky/Controllers/Inscription.swift
@@ -0,0 +1,17 @@
+//
+//  Inscription.swift
+//  Chicky
+//
+//  Created by Mac-Mini_2021 on 08/11/2021.
+//
+
+import UIKit
+
+struct Inscription: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+Ë
