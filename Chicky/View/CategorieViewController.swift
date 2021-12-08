1d29da5c11202ffaadc315b785b4f9000c10e7f1
Maher Ali <maher.ali@esprit.tn>
2021-12-08 20:44:23 +0100

    Map

---
+++ Chicky/View/CategorieViewController.swift
@@ -0,0 +1,67 @@
+//
+//  CategorieViewController.swift
+//  Chicky
+//
+//  Created by Jamel & Maher on 8/12/2021.
+//
+
+import UIKit
+
+class CategorieViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
+    
+
+    
+    private var categorie : [String] = ["Cafe","University","Hotel","Bar","FitnessCenter","Airport"]
+    var cat : String = "" 
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        categorie.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mCell")
+        let contentView = cell?.contentView
+        
+        let imageCategorie = contentView?.viewWithTag(2) as! UIImageView
+        let labelCategorie = contentView?.viewWithTag(1) as! UILabel
+        //let labellastMessage = contentView?.viewWithTag(3) as! UILabel
+      
+        
+        imageCategorie.image = UIImage(named: categorie[indexPath.row])
+        labelCategorie.text = categorie[indexPath.row]
+        
+        return cell!
+        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+          cat = categorie[indexPath.row]
+      
+        performSegue(withIdentifier: "map", sender: cat)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // let destination = segue.destination as! InscriptionSuivantView
+      //  destination.utilisateur = utilisateur
+        let destination = segue.destination as! SocialView
+        destination.search = cat
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
--- Chicky/View/SocialView.swift
+++ Chicky/View/SocialView.swift
@@ -6,24 +6,111 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class SocialView: UIViewController {
 
+class SocialView: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager:CLLocationManager!
+    var currentLocationStr = "Current location"
     // VAR
+    var search: String = ""
+    
+    //Iboutlets
+    @IBOutlet weak var Map: MKMapView!
     
     
     // WIDGET
     
     
     // PROTOCOLS
+
+    
+ 
     
     
     // LIFECYCLE
     override func viewDidLoad() {
         super.viewDidLoad()
+       // getNearByLandmarks()
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+           determineCurrentLocation()
+       }
+    
+    
+    @IBAction func btn_GetLocation(_ sender: UIButton) {
+     
+        
+    }
+    
+    
+    
     // METHODS
+    func getNearByLandmarks() {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = self.search
+        
+        let search = MKLocalSearch(request: request)
+        search.start{(response, error) in
+            if let response = response {
+                let mapItems = response.mapItems
+                print(mapItems)
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let mUserLocation:CLLocation = locations[0] as CLLocation
+            let center = CLLocationCoordinate2D(latitude: mUserLocation.coordinate.latitude, longitude: mUserLocation.coordinate.longitude)
+            let mRegion = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+            Map.setRegion(mRegion, animated: true)
+
+            // Get user's Current Location and Drop a pin
+        let mkAnnotation: MKPointAnnotation = MKPointAnnotation()
+            mkAnnotation.coordinate = CLLocationCoordinate2DMake(mUserLocation.coordinate.latitude, mUserLocation.coordinate.longitude)
+            mkAnnotation.title = self.setUsersClosestLocation(mLattitude: mUserLocation.coordinate.latitude, mLongitude: mUserLocation.coordinate.longitude)
+            Map.addAnnotation(mkAnnotation)
+        }
+    
+    func setUsersClosestLocation(mLattitude: CLLocationDegrees, mLongitude: CLLocationDegrees) -> String {
+        let geoCoder = CLGeocoder()
+        let location = CLLocation(latitude: mLattitude, longitude: mLongitude)
+
+        geoCoder.reverseGeocodeLocation(location) {
+            (placemarks, error) -> Void in
+
+            if let mPlacemark = placemarks{
+                if let dict = mPlacemark[0].addressDictionary as? [String: Any]{
+                    if let Name = dict["Name"] as? String{
+                        if let City = dict["City"] as? String{
+                            self.currentLocationStr = Name + ", " + City
+                        }
+                    }
+                }
+            }
+        }
+        return currentLocationStr
+    }
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+            print("Error - locationManager: \(error.localizedDescription)")
+        }
+    //MARK:- Intance Methods
+
+    func determineCurrentLocation() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+    }
+
+    
     
     
     // ACTIONS
